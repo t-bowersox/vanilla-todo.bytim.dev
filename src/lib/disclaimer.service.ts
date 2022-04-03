39aece5e5b97e8826e0f5eb5fc57405b6861394f
Tim Bowersox <11789189+t-bowersox@users.noreply.github.com>
2022-04-03 11:03:23 -0400

    feat: create DisclaimerService

---
+++ src/lib/disclaimer.service.ts
@@ -0,0 +1,32 @@
+export class DisclaimerService {
+  private static setHideDisclaimerToggle(): void {
+    const disclaimer = document.getElementById("disclaimer");
+    if (!disclaimer) {
+      return;
+    }
+
+    const dismissBtn = disclaimer.shadowRoot?.querySelector(".alert-dismiss-btn");
+    if (!dismissBtn) {
+      return;
+    }
+
+    dismissBtn.addEventListener("click", () => {
+      window.localStorage.setItem("hideDisclaimer", "true");
+    });
+  }
+
+  public static showDisclaimer(): void {
+    const disclaimer = document.getElementById("disclaimer");
+    if (!disclaimer) {
+      return;
+    }
+
+    if (window.localStorage.getItem("hideDisclaimer") === "true") {
+      disclaimer.remove();
+      return;
+    }
+
+    disclaimer.classList.remove("hidden");
+    this.setHideDisclaimerToggle();
+  }
+}
