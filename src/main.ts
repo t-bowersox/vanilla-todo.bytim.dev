09a09a3154eab205ea6e93ded1c2c26a3226d3c5
Tim Bowersox <11789189+t-bowersox@users.noreply.github.com>
2022-04-03 11:04:34 -0400

    feat: create bootstrap function in main.ts
    Replace default script generated by Vite.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import './style.css'
+import { AlertComponent } from "./lib/alert.component";
+import { DisclaimerService } from "./lib/disclaimer.service";
+import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+export function bootstrap(): void {
+  customElements.define("app-alert", AlertComponent);
+  DisclaimerService.showDisclaimer();
+}
